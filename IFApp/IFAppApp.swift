bb700539c6b4708b93207415339223ebe33b8006
Leonid Nifantiev <leonid.nifantiev@skelar.tech>
2024-10-30 19:00:05 +0200

    Initial Commit

---
+++ IFApp/IFAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IFAppApp.swift
+//  IFApp
+//
+//  Created by Leonid-user on 30.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct IFAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
